757ae52b1683aba5dac8aed1ad14e94026cd9e16
kataya1 <34242491+kataya1@users.noreply.github.com>
2020-02-10 20:59:14 +0200

    'f'

---
+++ cardsOO.py
@@ -0,0 +1,21 @@
+class CardGame:
+    pass
+
+class Suits:
+    suits = ('♠', '♥', '♦', '♣')
+
+class Cards:
+    def __init__(self,v,s):
+        self.value = v
+        self.suit = s
+
+
+class Deck:
+    pass
+
+class Hand:
+    pass
+
+class Numbers:
+    values = ('Ace', 'King', 'Queen', 'Jack', 10, 9, 8, 7, 6, 5, 4, 3, 2)
+
